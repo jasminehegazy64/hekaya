a34be35bd9a9248db9468f916b0a44663fc3ad87
jasmine <jasmine2004084@miuegypt.edu.eg>
2024-05-17 22:33:59 +0300

    basics

--- lib/common_widget/top_picks_cell.dart
+++ lib/common_widget/top_picks_cell.dart
@@ -1,22 +1,20 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_rating_bar/flutter_rating_bar.dart';
 
 import 'lib/common/color_extension.dart';
 
-class BestSellerCell extends StatelessWidget {
-  final Map bObj;
-  const BestSellerCell({super.key, required this.bObj});
+class TopPicksCell extends StatelessWidget {
+  final Map iObj;
+  const TopPicksCell({super.key, required this.iObj});
 
   @override
   Widget build(BuildContext context) {
     var media = MediaQuery.of(context).size;
-    return Container(
-        margin: const EdgeInsets.symmetric(horizontal: 12),
+    return SizedBox(
         // color: Colors.red,
         width: media.width * 0.32,
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisSize: MainAxisSize.min,
           children: [
             Container(
               decoration: BoxDecoration(
@@ -31,7 +29,7 @@ class BestSellerCell extends StatelessWidget {
               child: ClipRRect(
                 borderRadius: BorderRadius.circular(10),
                 child: Image.asset(
-                  bObj["img"].toString(),
+                  iObj["img"].toString(),
                   width: media.width * 0.32,
                   height: media.width * 0.50,
                   fit: BoxFit.cover,
@@ -42,44 +40,21 @@ class BestSellerCell extends StatelessWidget {
               height: 15,
             ),
             Text(
-              bObj["name"].toString(),
+              iObj["name"].toString(),
               maxLines: 3,
-              textAlign: TextAlign.left,
+              textAlign: TextAlign.center,
               style: TextStyle(
                   color: TColor.text,
                   fontSize: 13,
                   fontWeight: FontWeight.w700),
             ),
-            const SizedBox(
-              height: 8,
-            ),
             Text(
-              bObj["author"].toString(),
+              iObj["author"].toString(),
               maxLines: 1,
-              textAlign: TextAlign.left,
+              textAlign: TextAlign.center,
               style: TextStyle(
                 color: TColor.subTitle,
                 fontSize: 11,
-              ),
-            ),
-            const SizedBox(
-              height: 8,
-            ),
-            IgnorePointer(
-              ignoring: true,
-              child: RatingBar.builder(
-                initialRating: double.tryParse(bObj["rating"].toString()) ?? 1,
-                minRating: 1,
-                direction: Axis.horizontal,
-                allowHalfRating: true,
-                itemCount: 5,
-                itemSize: 15,
-                itemPadding: const EdgeInsets.symmetric(horizontal: 1.0),
-                itemBuilder: (context, _) => Icon(
-                  Icons.star,
-                  color: TColor.primary,
-                ),
-                onRatingUpdate: (rating) {},
               ),
             )
           ],
